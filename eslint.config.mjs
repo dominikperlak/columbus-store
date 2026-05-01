0cfe9915afb2e401fa095cfe85dea5778ba9597e
dominikperlak <57863768+dominikperlak@users.noreply.github.com>
2026-05-01 18:06:32 +0200

    feat: add api types from schema, fetch function,eslint and pr for github workflows

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,6 +13,18 @@ const eslintConfig = defineConfig([
     "build/**",
     "next-env.d.ts",
   ]),
+  {
+    files: ["**/*.ts", "**/*.tsx", "**/*.mjs"],
+    rules: {
+      quotes: ["error", "double"],
+      "jsx-quotes": ["error", "prefer-double"],
+      "no-trailing-spaces": "error",
+      "no-multiple-empty-lines": ["error", { max: 1, maxEOF: 0 }],
+      "no-console": "warn",
+      eqeqeq: "error",
+      "no-unused-vars": "error",
+    },
+  },
 ]);
 
 export default eslintConfig;
